de635c95b50f5c32e7e0f2de36c9fa27ed7f9dc7
magcen <149819491+magcen@users.noreply.github.com>
2024-01-14 13:10:20 +0100

    Added variables to the function for showing times for selected cities

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 function showSelectedCity(event) {
+  let londonTime = moment()
+    .tz("Europe/London")
+    .format("[It is currently] dddd, MMMM D, YYYY [in Europe/London.]");
+  let sydneyTime = moment()
+    .tz("Australia/Sydney")
+    .format("[It is currently] dddd, MMMM D, YYYY [in Australia/Sydney.]");
+  let tokyoTime = moment()
+    .tz("Asia/Tokyo")
+    .format("[It is currently] dddd, MMMM D, YYYY [in Asia/Tokyo.]");
   if (event.target.value === "london") {
-    alert("hello");
+    alert(londonTime);
   }
-  if (event.target.value === "rome") {
-    alert("ciao");
+  if (event.target.value === "sydney") {
+    alert(sydneyTime);
   }
-  if (event.target.value === "warsaw") {
-    alert("cześć");
+  if (event.target.value === "tokyo") {
+    alert(tokyoTime);
   }
 }
 
