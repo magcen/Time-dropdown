1db620f22b17f1dfdf714cda65631ea178048816
magcen <149819491+magcen@users.noreply.github.com>
2024-01-14 12:48:58 +0100

    Added an alerting function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-//define event listeners with alerts, podpiac przez queryselector event listery, 
+function showSelectedCity(event) {
+  if (event.target.value === "london") {
+    alert("hello");
+  }
+  if (event.target.value === "rome") {
+    alert("ciao");
+  }
+  if (event.target.value === "warsaw") {
+    alert("cześć");
+  }
+}
+
+let citySelect = document.querySelector("#cities");
+console.log(citySelect.value);
+citySelect.addEventListener("change", showSelectedCity);
